f422544a23736655151caa23c6e122d031cdcb76
Chloe Read <dev@readiefur.com>
2026-09-19 13:03:36 +0100

    chore: enforce dynamic updates on abstract OssmDevice

--- src/lib/services/OssmDevice.svelte.ts
+++ src/lib/services/OssmDevice.svelte.ts
@@ -1,5 +1,6 @@
 import { OssmStatus } from '$lib/ossm-ble-web/src/ossmBle';
 import type { OssmState, OssmPattern } from 'ossm-ble-web';
+import { PatternHelper } from 'ossm-ble-web';
 
 export type LifecycleState = 'uninitialized' | 'initializing' | 'ready' | 'disposed';
 export type ConnectionState = 'disconnected' | 'reconnecting' | 'connected';
@@ -14,24 +15,39 @@ export const EmptyOssmState: OssmState = {
 
 export abstract class OssmDevice {
     // Instance properties
-    abstract lifecycleState: LifecycleState;
-    abstract statusMessage: string | null;
-    abstract errorMessage: string | null;
+    abstract get lifecycleState(): LifecycleState;
+    abstract get statusMessage(): string | null;
+    abstract get errorMessage(): string | null;
 
     // Device properties
-    abstract connectionState: ConnectionState;
-    abstract currentState: OssmState;
-    abstract patterns: OssmPattern[];
+    abstract get connectionState(): ConnectionState;
+    abstract get currentState(): OssmState;
+    abstract get patterns(): OssmPattern[];
 }
 
 export class DummyOssmDevice extends OssmDevice {
-    lifecycleState: LifecycleState = 'ready'
-    statusMessage: string | null = null;
-    errorMessage: string | null = null;
-    connectionState: ConnectionState = 'connected';
-    currentState: OssmState = EmptyOssmState;
-    patterns: OssmPattern[] = [
+    lifecycleState: LifecycleState = $state('ready');
+    statusMessage: string | null = $state(null);
+    errorMessage: string | null = $state(null);
+    connectionState: ConnectionState = $state('connected');
+    currentState: OssmState = $state({
+        status: OssmStatus.StrokeEngineIdle,
+        ...new PatternHelper(0, 15, 60, 30, 50, true)
+    });
+    patterns: OssmPattern[] = $state([
         { idx: 0, name: 'Pattern 1', description: 'Lorem ipsum dolor sit amet' },
         { idx: 1, name: 'Pattern 2', description: 'consectetur adipiscing elit' }
-    ];
+    ]);
+
+    constructor(doDynamicUpdate: boolean = false) {
+        super();
+        if (doDynamicUpdate) {
+            setTimeout(() => {
+                this.currentState = {
+                    status: OssmStatus.StrokeEnginePattern,
+                    ...new PatternHelper(0, 5, 25, 70, 40, false)
+                };
+            }, 2000);
+        }
+    }
 }
